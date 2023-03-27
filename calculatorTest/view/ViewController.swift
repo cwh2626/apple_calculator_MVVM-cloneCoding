c54ed40301c1962ab2a14b645e1c6a9eb9dc4fb7
cwh2626 <cwh2626@gmail.com>
2023-03-27 19:18:19 +0900

    Refactor source code using RxSwift and RxCocoa

--- calculatorTest/view/ViewController.swift
+++ calculatorTest/view/ViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-import Combine
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
@@ -29,40 +30,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var zeroButton: UIButton!
     @IBOutlet weak var decimalButton: UIButton!
     @IBOutlet weak var resultButton: UIButton!
-    
     @IBOutlet weak var resultLabel: UILabel!
     
     private var viewModel = CalculatorViewModel() // 뷰 모델 인스턴스
-    // Published를 구독후에 반환되는 Cancellable을 저장하는 변수
-    private var cancellables = Set<AnyCancellable>()
-    // combine은 KVO처럼 과거의 값을 가져올수없기에 view단에서 따로 이전값을 설정
+    
+    // conbine의.store(in: &cancellables) 와 비슷한기능이다 disposeBag 담아 두었다가 해당 변수가 deinit 타이밍에 dispose 하는 구조이다.
+    // Observable의 메모리 누수 방지를 위한 자동 구독해지 기능이라고 생각하면 편할듯
+    private let disposeBag = DisposeBag()
+    
     private var activatedButton: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setButton()
-        
-        // ViewModel의 displayText가 변경될 때마다 클로저 함수 발생
-        // $displayText: 발행자(publisher)
-        // .sink : 구독자(Subscriber)가 클로저형태로 발행자와 연결하는거
-        // .store : 구독을 관리하는 Cancellable이라는 객체를 저장하는 느낌 이렇게 set이라는 변수에 저장하는 이유는
-        //          저장하지않으면 즉시 구독이 취소되어 클로저가 실행이 안되기 떄문이다
-        //          그래서 set객체가 수명이 다하면 자동으로 구독이 다 취소되는듯하다 나의 생각 ㅎ
-        viewModel.$displayText
-                    .sink { [weak self] value in
-                        self?.resultLabel.text = value
-                    }
-                    .store(in: &cancellables)
-        
-        viewModel.$operation
-                    .sink { [weak self] value in
-                        guard let _self = self else{ return }
-                        _self.operationButtonToggle(new: false,value: _self.activatedButton)
-                        _self.operationButtonToggle(new: true,value: value)
-                    }
-                    .store(in: &cancellables)
+        bindViewModel()
     }
+   
     
+    func bindViewModel() {
+        // displayText 과 self.resultLabel.text를 바인딩하는 코드
+        // .bind : viewModel.displayText의 값이 변경될떄 마다 self.resultLabel.rx.text도 같은 값으로 변경됨 (rx란 UIkit컴포넌트에 Observable 구조체와 연결하게 해주는 역할)
+        // .disposed: 바인드후 Disposable을 방출하는데 이걸 disposeBag 에 담아주는 역할 _ 메모리 자동 해지를 위해 (자동구독해지)
+        viewModel.displayText
+            .bind(to: self.resultLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        // operation를 구독하는 기능
+        // .subscribe: operation을 구독해 해당 값이 변경될때마다 클로저함수가 발생 onNext는 바뀐값을 인자로 받아서 클로저를 실행함 이외에도 onError 등등 있음
+        viewModel.operation
+            .subscribe(onNext:{ [weak self] value in
+                guard let _self = self else{ return }
+                _self.operationButtonToggle(new: false,value: _self.activatedButton)
+                _self.operationButtonToggle(new: true,value: value)
+            })
+            .disposed(by: disposeBag)
+    }
     
     /// 사칙연산버튼 터치시 컬러변경처리
     /// - Parameters:
@@ -154,18 +156,18 @@ class ViewController: UIViewController {
     // 숫자 버튼이 눌렸을 때 호출되는 엑션이벤트
     @IBAction func numberTouch(_ sender: UIButton) {
         guard let number = sender.currentTitle else { return }
-        viewModel.inputNumber(number: number)
+        viewModel.numberButtonTapped.accept(number)
     }
     
     // 즉발성 부호 버튼이 눌렸을 때 호출되는 엑션이벤트
     @IBAction func operationButtonTouch(_ sender: UIButton) {
         guard let operation = sender.currentTitle else { return }
-        viewModel.inputOperation(operation: operation)
+        viewModel.operationButtonTapped.accept(operation)
     }
     
     // 연산자 부호 버튼이 눌렸을 때 호출되는 엑션이벤트
     @IBAction func arithmeticButtonTouch(_ sender: UIButton) {
         guard let operation = sender.currentTitle else { return }
-        viewModel.inputOperation(operation: operation)
+        viewModel.operationButtonTapped.accept(operation)
     }
 }
--- calculatorTest/viewModel/CalculatorViewModel.swift
+++ calculatorTest/viewModel/CalculatorViewModel.swift
@@ -1,32 +1,60 @@
 import Foundation
-import Combine
-// 계산기 뷰 모델 클래스 // ObservableObject : @Published을 선언하기위한 프로토콜
-class CalculatorViewModel: ObservableObject {
-    // @Published : 해당 변수를 발행하여 다른 객체에서 해당 변수를 구독하여 감시가 가능하다 바인딩시에 편의 기능
-    @Published var displayText: String = "0" // 화면에 표시할 텍스트
-    @Published var operation: String = ""
+import RxSwift
+import RxCocoa
+
+class CalculatorViewModel {
+    //1.BehaviorRelay:
+    //BehaviorRelay는 RxCocoa 라이브러리에 포함된 클래스로, 에러 및 완료 이벤트를 방출하지 않습니다. 대신에 가장 최근의 이벤트를 저장하고 새 구독자에게 방출해줍니다. 따라서 BehaviorRelay는 초기 값을 가지며, 언제든지 현재 값을 가져올 수 있습니다. 주로 UI 상태 관리에 사용됩니다.
+    //2/PublishRelay:
+    //PublishRelay는 역시 RxCocoa 라이브러리에 포함되어 있으며, 에러 및 완료 이벤트를 방출하지 않습니다. PublishRelay는 가장 최근의 값을 저장하지 않고, 구독 시점 이후에 발생하는 이벤트만 전달합니다. 따라서 PublishRelay는 초기 값을 가지지 않습니다.
+    //3.PublishSubject:
+    //PublishSubject는 RxSwift 라이브러리에 포함된 클래스로, 에러와 완료 이벤트를 처리할 수 있습니다. PublishSubject는 구독 이후에 발생하는 이벤트만 전달하며, 가장 최근의 값을 저장하지 않습니다. 초기 값을 가지지 않고, 주로 다양한 이벤트를 처리할 때 사용됩니다.
+    //4.BehaviorSubject:
+    //BehaviorSubject는 RxSwift 라이브러리에 포함되어 있으며, 에러와 완료 이벤트를 처리할 수 있습니다. BehaviorSubject는 가장 최근의 값을 저장하고 새 구독자에게 전달합니다. 초기 값을 가지며, 주로 초기 값이 필요한 상태 관리에 사용됩니다.
+    
+    // 위의 4개이외에도 더 다양하게 많다 우선은 이 정도만 정리하고 나중에 싹 모아서 정리릃 해볼까한다 우선은 노션에 올려둔 'RxSwift기초' 을 참고하도록하자
+    var displayText = BehaviorRelay<String>(value: "0")
+    var operation = BehaviorRelay<String>(value: "")
+    let numberButtonTapped = PublishRelay<String>()
+    let operationButtonTapped = PublishRelay<String>()
+    private let disposeBag = DisposeBag()
     private var isPerformingOperation: Bool = false
     private var hasDecimalPoint: Bool = false
     private var errorState: Bool = false
     private var calculator = Calculator() // Calculator 모델 인스턴스
     
+    init() {
+        numberButtonTapped
+            .subscribe(onNext: { [weak self] number in
+                self?.inputNumber(number: number)
+            })
+            .disposed(by: disposeBag)
+
+        operationButtonTapped
+            .subscribe(onNext: { [weak self] operation in
+                self?.inputOperation(operation: operation)
+            })
+            .disposed(by: disposeBag)
+    }
+    
     // 숫자 입력 함수
-    func inputNumber(number: String) {
+    private func inputNumber(number: String) {
         if isPerformingOperation || errorState {
-            displayText = number
+            displayText.accept(number)
             isPerformingOperation = false
             hasDecimalPoint = false
             errorState = false
-        } else if displayText.count < 9 {
-            if displayText == "0" {
-                displayText.removeAll()
+        } else if displayText.value.count < 9 {
+            if displayText.value == "0" {
+                displayText.accept(number)
+            } else {
+                displayText.accept(displayText.value + number)
             }
-            displayText += number
         }
     }
     
     // 연산자 입력 함수
-    func inputOperation(operation: String) {
+    private func inputOperation(operation: String) {
         guard !errorState || operation == "C" else { return }
         
         switch operation {
@@ -36,43 +64,42 @@ class CalculatorViewModel: ObservableObject {
                 guard !errorState else { return }
             }
             
-            calculator.setOperand(operand: Double(displayText)! ,calculateResult: false)
+            calculator.setOperand(operand: Double(displayText.value)! ,calculateResult: false)
             calculator.setOperation(operation: operation)
-            self.operation = calculator.operation
+            self.operation.accept(calculator.operation)
             isPerformingOperation = true
         case "C":
             // 초기화 버튼 - 모든 값을 초기화
-            displayText = "0"
+            displayText.accept("0")
             self.calculatorPropertyInitialization()
             isPerformingOperation = false
             hasDecimalPoint = false
             errorState = false
         case "+/-":
             // 부호 변경 버튼
-            let value = Double(displayText)!
+            let value = Double(displayText.value)!
             if value.truncatingRemainder(dividingBy: 1) == 0 {
-                displayText = String(format: "%.0f", -value)
-                
+                displayText.accept(String(format: "%.0f", -value))
             } else {
-                displayText = String(-value)
+                displayText.accept(String(-value))
             }
         case "%":
-            let value = Double(displayText)!
+            let value = Double(displayText.value)!
             let lastValue = value / 100.0
             
             if  lastValue.truncatingRemainder(dividingBy: 1) == 0 {
-                displayText = String(format: "%.0f", lastValue)
+                displayText.accept(String(format: "%.0f", lastValue))
                 hasDecimalPoint = false
             } else {
-                displayText = String(lastValue)
+                displayText.accept(String(lastValue))
                 hasDecimalPoint = true
             }
         case ".":
             if isPerformingOperation {
-                displayText = "0."
+                displayText.accept("0.")
                 isPerformingOperation = false
-            } else if displayText.count < 9 && !hasDecimalPoint {
-                displayText += "."
+            } else if displayText.value.count < 9 && !hasDecimalPoint {
+                displayText.accept(displayText.value + ".")
             }
             
             hasDecimalPoint = true
@@ -84,18 +111,18 @@ class CalculatorViewModel: ObservableObject {
     }
     
     // 결과 계산 함수
-    func calculateResult() {
+    private func calculateResult() {
         guard !calculator.operation.isEmpty else { return }
         
-        calculator.setOperand(operand: Double(displayText)! ,calculateResult: true)
+        calculator.setOperand(operand: Double(displayText.value)! ,calculateResult: true)
         
         if let result = calculator.calculate() {
             guard result.isFinite else { return self.stateError() }
             
             if result.truncatingRemainder(dividingBy: 1) == 0 {
-                displayText = String(format: "%.0f", result)
+                displayText.accept(String(format: "%.0f", result))
             } else {
-                displayText = String(result)
+                displayText.accept(String(result))
             }
             
             self.calculatorPropertyInitialization()
@@ -111,11 +138,11 @@ class CalculatorViewModel: ObservableObject {
         calculator.firstOperand = 0
         calculator.secondOperand = 0
         calculator.operation = ""
-        self.operation = calculator.operation
+        self.operation.accept(calculator.operation)
     }
     
     private func stateError() {
-        displayText = "Error"
+        displayText.accept("Error")
         self.calculatorPropertyInitialization()
         isPerformingOperation = false
         hasDecimalPoint = false
